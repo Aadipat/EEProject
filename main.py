2864b636de1829da42d17b5ff6e59db8bc7a533b
Aadi <aadikpat@gmail.com>
2022-04-30 21:30:56 +0200

    Updating Main

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ Cube = cube.Rubiks_Cube(2)
 
 Cube.show()
 
-print(Cube.piece_vectors)
 Cube.scramble(cube.select_random_transformations(Cube.Transformations_possible,2))
-# print(len(cube.pieces))
 
-print(Cube.piece_vectors)
 Cube.show()
